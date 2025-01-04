cc4189c92e2d5515b7f1c50dec2d8cc9eda0d6ab
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-04 13:57:33 +0200

    created index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import app from "./app";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
