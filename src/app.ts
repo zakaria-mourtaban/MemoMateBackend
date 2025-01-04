3a156728069edff6409264809d892032873bc1e9
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-04 14:24:28 +0200

    created auth apis

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ dotenv.config();
 connectDB();
 
 const app: Application = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 
@@ -16,7 +17,14 @@ app.use((req, res, next) => {
 });
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.status(500).json({ message: err.message });
-});
+	console.error("Error occurred:", err);          // Log the error object
+	console.error("Stack trace:", err.stack);       // Log the full stack trace
+  
+	// Send a response back to the client
+	res.status(500).json({
+	  message: err.message,                         // Show the error message to the client
+	  stack: process.env.ENV === 'development' ? err.stack : undefined // Optional: Show stack in development only
+	});
+  });
 
 export default app;
