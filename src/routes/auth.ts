8dfb352cc77ead431e7b1e443dba198b6d1c01a5
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-05 13:12:19 +0200

    moved my authentication functions to auth controller

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,66 +1,10 @@
 import express, { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import User, { IUser } from "../models/User";
+import { authenticateUser, registerUser } from "../controllers/auth";
 
 const router = express.Router();
 
-// Utility function to generate a JWT token
-const generateToken = (id: string): string => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || "", { expiresIn: "30d" });
-};
+router.post("/register", registerUser);
 
-// @route   POST /api/auth/register
-// @desc    Register a new user
-// @access  Public
-router.post("/register", async (req: Request, res: Response): Promise<any> => {
-  const { name, email, password } = req.body;
-
-  try {
-    const userExists: IUser | null = await User.findOne({ email });
-
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-	const user: IUser = await User.create({ name, email, password });
-	
-    if (user) {
-      res.status(201).json({
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(400).json({ message: "Invalid user data" });
-    }
-  } catch (error : any) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// @route   POST /api/auth/login
-// @desc    Login a user
-// @access  Public
-router.post("/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-
-    if (user && (await user.matchPassword(password))) {
-      res.json({
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(401).json({ message: "Invalid email or password" });
-    }
-  } catch (error : any) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.post("/login", authenticateUser );
 
 export default router;
