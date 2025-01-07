ea4cecbf4754bc1a4de06e9d3bd36764af84b1cc
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-08 01:42:27 +0200

    basic working version of the RAG chatbot

--- src/controllers/langchainRAG/indexing.ts
+++ src/controllers/langchainRAG/indexing.ts
@@ -7,72 +7,66 @@ import { Annotation, StateGraph } from "@langchain/langgraph";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { llm, vectorStore } from "./langchainRAG";
 
-
 // Load and chunk contents of blog
 const pTagSelector = "p";
-// cheerio takes a link and returns a traversable cheerio object where we can modify and search for what we need
 const cheerioLoader = new CheerioWebBaseLoader(
-  "https://lilianweng.github.io/posts/2023-06-23-agent/",
-  {
-    selector: pTagSelector
-  }
+	"https://lilianweng.github.io/posts/2023-06-23-agent/",
+	{
+		selector: pTagSelector,
+	}
 );
-//loading the website
+
 const docs = await cheerioLoader.load();
 
-
-//split the data in the p tags to chunck of 1000 characters each and 200 character chunk overlap
 const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 1000, chunkOverlap: 200
+	chunkSize: 1000,
+	chunkOverlap: 200,
 });
 const allSplits = await splitter.splitDocuments(docs);
 
-
-// Index chunks in the vector store
-await vectorStore.addDocuments(allSplits)
+// Index chunks
+await vectorStore.addDocuments(allSplits);
 
 // Define prompt for question-answering
 const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
 
 // Define state for application
 const InputStateAnnotation = Annotation.Root({
-  question: Annotation<string>,
+	question: Annotation<string>,
 });
 
 const StateAnnotation = Annotation.Root({
-  question: Annotation<string>,
-  context: Annotation<Document[]>,
-  answer: Annotation<string>,
+	question: Annotation<string>,
+	context: Annotation<Document[]>,
+	answer: Annotation<string>,
 });
 
 // Define application steps
 const retrieve = async (state: typeof InputStateAnnotation.State) => {
-  const retrievedDocs = await vectorStore.similaritySearch(state.question)
-  return { context: retrievedDocs };
+	const retrievedDocs = await vectorStore.similaritySearch(state.question);
+	return { context: retrievedDocs };
 };
-
 
 const generate = async (state: typeof StateAnnotation.State) => {
-  const docsContent = state.context.map(doc => doc.pageContent).join("\n");
-  const messages = await promptTemplate.invoke({ question: state.question, context: docsContent });
-  const response = await llm.invoke(messages);
-  return { answer: response.content };
+	const docsContent = state.context.map((doc) => doc.pageContent).join("\n");
+	const messages = await promptTemplate.invoke({
+		question: state.question,
+		context: docsContent,
+	});
+	const response = await llm.invoke(messages);
+	return { answer: response.content };
 };
-
 
 // Compile application and test
 const graph = new StateGraph(StateAnnotation)
-  .addNode("retrieve", retrieve)
-  .addNode("generate", generate)
-  .addEdge("__start__", "retrieve")
-  .addEdge("retrieve", "generate")
-  .addEdge("generate", "__end__")
+	.addNode("retrieve", retrieve)
+	.addNode("generate", generate)
+	.addEdge("__start__", "retrieve")
+	.addEdge("retrieve", "generate")
+	.addEdge("generate", "__end__")
 	.compile();
-  
-//this graph connects the flow steps from __start__ (defined in the library) to __end__ implementing the nodes we created
 
 let inputs = { question: "What is Task Decomposition?" };
 
 const result = await graph.invoke(inputs);
-console.log(result.context.slice(0, 2));
-console.log(`\nAnswer: ${result["answer"]}`);
+console.log(result.answer);
