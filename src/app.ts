c0ea0fc4439438a942c9723f838cad40157448b0
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-14 16:41:06 +0200

    removed conflicting path names leading to auth routes being protected

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,39 @@ import express, { Application } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import authRoutes from "./routes/auth";
-import workspaceRoutes from "./routes/workspace"
+import workspaceRoutes from "./routes/workspace";
+import cors from "cors";
+import { protect } from "./middleware/auth";
 
 dotenv.config();
 connectDB();
 
 const app: Application = express();
-app.use(express.json({ limit: '10mb' }));
+
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/", workspaceRoutes)
+app.use("/auth/", authRoutes);
+app.use("/api/", protect, workspaceRoutes);
 app.use((req, res, next) => {
-  res.status(404).json({ message: "Not Found" });
+	res.status(404).json({ message: "Not Found" });
 });
 
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-	console.error("Error occurred:", err);          // Log the error object
-	console.error("Stack trace:", err.stack);       // Log the full stack trace
-  
-	// Send a response back to the client
-	res.status(500).json({
-	  message: err.message,                         // Show the error message to the client
-	  stack: process.env.ENV === 'development' ? err.stack : undefined // Optional: Show stack in development only
-	});
-  });
+app.use(
+	(
+		err: Error,
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		console.error("Error occurred:", err);
+		console.error("Stack trace:", err.stack);
+
+		res.status(500).json({
+			message: err.message,
+			stack: process.env.ENV === "development" ? err.stack : undefined,
+		});
+	}
+);
 
 export default app;
--- src/routes/workspace.ts
+++ src/routes/workspace.ts
@@ -10,20 +10,12 @@ import uploadMiddleware from "../middleware/upload";
 
 const router = express.Router();
 
-// Apply authentication middleware to all routes
-router.use(protect);
-
-// Create a new workspace
 router.post("/workspace", createWorkspace);
 
-// Fetch a specific workspace
 router.get("/workspace/:id", fetchWorkspace);
 
-// Add a new node to workspace
 router.post("/workspace/:id/add", uploadMiddleware, addToWorkspace);
 
-
-// Delete node from workspace
 router.put("/workspace/:id/delete", deleteFromWorkspace);
 
 export default router;
