c70f1e0916963195aaedbced01201c2acecc66db
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-16 00:42:19 +0200

    updated upload limit

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ connectDB();
 
 const app: Application = express();
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth/", authRoutes);
