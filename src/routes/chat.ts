d9cb2bdf201549efe213ed3f52950484537bf7ed
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-19 21:15:40 +0200

    added api route

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import { protect } from "../middleware/auth";
-import { diagramPrompt, getalltext } from "../controllers/chats";
+import { diagramPrompt, getalltext, queryChatVectorStore } from "../controllers/chats";
 
 const router = express.Router();
 
 router.post("/chat/diagram", diagramPrompt)
 router.get("/chat/all/:id", getalltext)
+router.post("/chat/:id/call", queryChatVectorStore)
 export default router
