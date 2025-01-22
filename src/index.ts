edb35fa2c36f9849ed674ebdc74212ea8596d26b
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-22 23:13:39 +0200

    updated code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
